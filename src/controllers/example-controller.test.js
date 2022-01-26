67f6566eba07e1d094f5bf20f4f101f5395ce9a2
LB Johnston <lb.johnston@virginaustralia.com>
2022-01-26 13:07:57 +1000

    update controller test to validate DOM mutation

--- src/controllers/example-controller.test.js
+++ src/controllers/example-controller.test.js
@@ -1,7 +1,18 @@
+import { Application } from '@hotwired/stimulus';
+
 import ExampleController from './example-controller';
 
 describe('ExampleController', () => {
+  document.body.innerHTML = `<h1 data-controller="example" id="header"></h1>`;
+
+  const application = Application.start();
+  application.register('example', ExampleController);
+
   it('should exist', () => {
     expect(ExampleController).toBeInstanceOf(Function);
+  });
+
+  it('should update the content of the controlled element', () => {
+    expect(document.getElementById('header').textContent).toEqual('It works!');
   });
 });
