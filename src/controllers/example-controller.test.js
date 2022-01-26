7b9906461ad504d3eb16b313b7cc64f474c85b3e
LB Johnston <lb.johnston@virginaustralia.com>
2022-01-26 12:46:33 +1000

    add initial test (basic) & install jest

---
+++ src/controllers/example-controller.test.js
@@ -0,0 +1,7 @@
+import ExampleController from "./example-controller";
+
+describe("ExampleController", () => {
+  it("should exist", () => {
+    expect(ExampleController).toBeInstanceOf(Function);
+  });
+});
