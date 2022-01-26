80a8267e72a3037d24f20834453cbf792e974de2
LB Johnston <lb.johnston@virginaustralia.com>
2022-01-26 12:58:40 +1000

    configure eslint

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { Application } from '@hotwired/stimulus';
 import { definitionsFromContext } from '@hotwired/stimulus-webpack-helpers';
 
 const application = Application.start();
+// eslint-disable-next-line no-undef
 const context = require.context('./controllers', true, /\.js$/);
 
 application.load(definitionsFromContext(context));
