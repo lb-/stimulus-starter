abcb82d7e8ddad44b084f617357b82676a9c36d7
LB Johnston <lb.johnston@virginaustralia.com>
2022-02-16 11:27:20 +1100

    basic functionality implemented

--- src/controllers/expanding-formset-controller.js
+++ src/controllers/expanding-formset-controller.js
@@ -1,107 +1,274 @@
 import { Controller } from '@hotwired/stimulus';
 import TokenList from '@wordpress/token-list';
 
+const SELECTORS = {
+  idInput: '[name$="-id"]',
+  orderInput: '[name$="-ORDER"]',
+  deleteInput: '[name$="-DELETE"]',
+  moveUpButton: '[data-action$="#moveUp"]',
+  moveDownButton: '[data-action$="#moveDown"]',
+  deleteButton: '[data-action$="#delete"]',
+};
+
 class ExpandingFormset extends Controller {
-  static classes = ['isMoving'];
+  static classes = [
+    'animate',
+    'animateDeleted',
+    'animateMoveUp',
+    'animateMoveDown',
+    'isDeleted',
+    'isMoving',
+  ];
 
   static values = {
+    itemSelectors: { default: { ...SELECTORS }, type: Object },
     initial: { default: 0, type: Number },
     min: { default: 0, type: Number },
     max: { default: 1000, type: Number },
+    noAnimate: { default: false, type: Boolean },
     total: { default: 0, type: Number },
   };
 
   static targets = ['addButton', 'container', 'deleted', 'item', 'template'];
 
-  addItem() {
-    this.containerTarget.appendChild(this.getNewPanelFromTemplate());
-  }
+  add() {
+    const newItem = this.getNewItemFromTemplate();
 
-  animateSwap(item1, item2) {
-    const parent = self.formsUl;
-    const children = parent.children('li:not(.deleted)');
-
-    // Apply moving class to container (ul.multiple) so it can assist absolute positioning of it's children
-    // Also set it's relatively calculated height to be an absolute one,
-    // to prevent the container collapsing while its children go absolute
-    this.element.classList.add(this.isMovingClass);
-    this.element.style.height = this.element.height();
-
-    this.itemTargets.forEach((item) => {
-      item.style.top = item.position.top;
-      item.classList.add(this.isMovingClass);
-    });
-    // .each(function moveChildTop() {
-    //   $(this).css('top', $(this).position().top);
-    // })
-    // .addClass('moving');
-
-    // animate swapping around
-    item1.animate({ top: item2.position().top }, 200, () => {
-      parent.removeClass('moving').removeAttr('style');
-      children.removeClass('moving').removeAttr('style');
+    const event = this.dispatch('add', {
+      cancelable: true,
+      detail: { newItem },
     });
 
-    item2.animate({ top: item1.position().top }, 200, () => {
-      parent.removeClass('moving').removeAttr('style');
-      children.removeClass('moving').removeAttr('style');
-    });
+    if (event.defaultPrevented) return;
+
+    this.containerTarget.appendChild(newItem);
   }
 
   connect() {
-    setTimeout(() => {
-      this.update();
+    this.itemTargets.forEach((item) => {
+      item.refs = this.getItemRefs(item);
     });
   }
 
-  itemTargetConnected() {
-    this.update();
+  getItemWithTarget(target) {
+    const itemTargets = this.itemTargets;
+    const index = itemTargets.findIndex((element) => element.contains(target));
+    if (index === -1) return [null, -1];
+    return [itemTargets[index], index];
   }
 
-  deletedTargetConnected() {
-    this.update();
+  getItemRefs(item) {
+    // this will cause issues if the current formset does NOT allow deleting but a nested one does
+    const querySelector = (selector) => item.querySelector(selector);
+
+    // allow for default values inside object
+    const selectors = { ...SELECTORS, ...this.itemSelectorsValue };
+
+    return {
+      idInput: querySelector(selectors.idInput),
+      orderInput: querySelector(selectors.orderInput),
+      deleteInput: querySelector(selectors.deleteInput),
+      moveUpButton: querySelector(selectors.moveUpButton),
+      moveDownButton: querySelector(selectors.moveDownButton),
+      deleteButton: querySelector(selectors.deleteButton),
+    };
   }
 
-  deleteItem({ target }) {
-    const item = this.itemTargets.find((element) => element === target);
-    const targetAttributeKey = `data-${this.identifier}-target`;
-    const targets = new TokenList(item.getAttribute(targetAttributeKey));
-    targets.add('deleted');
-    item.setAttribute(targetAttributeKey, targets.toString());
-  }
-
-  moveItem({ target, detail: { moveUp } }) {
-    const item = this.itemTargets.find((element) => element === target);
-    console.log('moveItem', { item, moveUp });
-  }
-
-  update() {
-    // update values and element state
-    // always use the DOM as the source of truth where possible
-
-    const total = this.itemTargets.length - this.deletedTargets.length;
-    this.addButtonTarget.disabled = total >= this.maxValue;
-    this.totalValue = total;
-
-    this.itemTargets.forEach((target, index) => {
-      this.dispatch('update', {
-        detail: {
-          index,
-          maxValue: this.maxValue,
-          minValue: this.minValue,
-          total,
-        },
-        target,
-      });
-    });
-  }
-
-  getNewPanelFromTemplate() {
+  getNewItemFromTemplate() {
     const nextId = this.containerTarget.childElementCount + 1;
     const template = this.templateTarget;
     const newPanel = template.content.firstElementChild.cloneNode(true);
     newPanel.innerHTML = newPanel.innerHTML.replaceAll('__prefix__', nextId);
     return newPanel;
+  }
+
+  itemTargetConnected(item) {
+    item.refs = this.getItemRefs(item);
+
+    this.update(() => {
+      if (this.isMoving) return; // might be a nicer way to avoid this when moving
+      this.dispatch('added', { cancelable: false, detail: { item } });
+    });
+  }
+
+  itemTargetDisconnected(item) {
+    console.log('item disconnected', { item, isMoving: this.isMoving });
+    this.update(() => {
+      if (this.isMoving) return; // might be a nicer way to avoid this when moving
+      this.dispatch('deleted', { cancelable: false, detail: { item } });
+    });
+  }
+
+  delete({ target }) {
+    const [item, index] = this.getItemWithTarget(target);
+
+    const event = this.dispatch('delete', {
+      cancelable: true,
+      detail: { index },
+      target: item,
+    });
+
+    if (event.defaultPrevented) return;
+
+    if (this.noAnimateValue) {
+      item.classList.add(this.isDeletedClass);
+    } else {
+      // we may not want to animate deletion - it is a bit jarring
+
+      const animateClass = this.animateClass;
+      const animateDeleted = this.animateDeletedClass;
+
+      item.style.position = 'relative';
+      item.classList.add(animateClass);
+      item.classList.add(animateDeleted);
+
+      item.addEventListener(
+        'animationend',
+        () => {
+          item.classList.add(this.isDeletedClass);
+          item.classList.remove(animateClass);
+          item.classList.remove(animateDeleted);
+
+          delete item.style.position;
+        },
+        { capture: true, once: true, passive: true }
+      );
+    }
+
+    const {
+      moveUpButton,
+      moveDownButton,
+      deleteButton,
+      deleteInput,
+      orderInput,
+    } = item.refs;
+
+    // update inputs to reflect that this is deleted
+    // TODO - if the item has been added client-side, we can just delete the DOM attribute (unless we want to provide undo)
+    deleteInput.value = 1;
+    if (moveUpButton) moveUpButton.disabled = true;
+    if (moveDownButton) moveDownButton.disabled = true;
+    if (deleteButton) deleteButton.disabled = true;
+    if (orderInput) orderInput.value = -1;
+
+    // update targets on item to remove 'item' and add 'deleted'
+
+    const targetAttributeKey = `data-${this.identifier}-target`;
+
+    const targets = new TokenList(
+      item.getAttribute(`data-${this.identifier}-target`)
+    );
+
+    targets.add('deleted');
+    targets.remove('item');
+
+    item.setAttribute(targetAttributeKey, targets.toString());
+  }
+
+  moveUp({ target }) {
+    const [item, index] = this.getItemWithTarget(target);
+    if (index <= 0) return;
+
+    const event = this.dispatch('moveUp', {
+      cancelable: true,
+      detail: { index },
+      target: item,
+    });
+
+    if (event.defaultPrevented) return;
+
+    const itemsToSwap = [item, this.itemTargets[index - 1]];
+
+    this.transitionSwap(itemsToSwap);
+
+    this.update(() => {
+      this.dispatch('movedUp', {
+        target: item,
+        detail: { index, cancelable: false },
+      });
+    });
+  }
+
+  moveDown({ target }) {
+    const [item, index] = this.getItemWithTarget(target);
+    if (index >= this.totalValue - 1) return;
+
+    const event = this.dispatch('moveDown', {
+      cancelable: true,
+      detail: { index },
+      target: item,
+    });
+
+    if (event.defaultPrevented) return;
+
+    const itemsToSwap = [this.itemTargets[index + 1], item];
+    this.transitionSwap(itemsToSwap);
+
+    this.update(() => {
+      this.dispatch('movedUp', {
+        target: item,
+        detail: { index, cancelable: false },
+      });
+    });
+  }
+
+  transitionSwap(itemsToSwap) {
+    this.isMoving = true;
+    const isMovingClass = this.isMovingClass;
+    this.element.classList.add(this.isMovingClass);
+
+    // swap the actual DOM elements first
+    this.containerTarget.insertBefore(...itemsToSwap);
+
+    if (this.noAnimateValue) {
+      this.isMoving = false;
+      return;
+    }
+
+    const animateClass = this.animateClass;
+    const animateMoveUp = this.animateMoveUpClass;
+    const animateMoveDown = this.animateMoveDownClass;
+
+    itemsToSwap.forEach((itemToSwap, index) => {
+      // TODO - check if we can do this in bulk
+
+      itemToSwap.classList.add(animateClass);
+      itemToSwap.classList.add(index === 0 ? animateMoveUp : animateMoveDown);
+
+      itemToSwap.addEventListener(
+        'animationend',
+        () => {
+          this.element.classList.remove(isMovingClass);
+          itemToSwap.classList.remove(animateClass);
+          itemToSwap.classList.remove(animateMoveUp);
+          itemToSwap.classList.remove(animateMoveDown);
+          this.isMoving = false;
+        },
+        { capture: true, once: true, passive: true }
+      );
+    });
+  }
+
+  update(afterUpdateFn = null) {
+    const minValue = this.minValue;
+    const total = this.itemTargets.length;
+
+    // update main controller values and state
+    this.totalValue = total;
+    this.addButtonTarget.disabled = total >= this.maxValue;
+
+    this.itemTargets.forEach((item, index) => {
+      const { moveUpButton, moveDownButton, deleteButton, orderInput } =
+        item.refs;
+
+      // update inner input values & button states
+      if (orderInput) orderInput.value = index + 1;
+      if (moveUpButton) moveUpButton.disabled = index === 0;
+      if (moveDownButton) moveDownButton.disabled = index === total - 1;
+      if (deleteButton) deleteButton.disabled = total <= minValue;
+    });
+
+    afterUpdateFn && setTimeout(afterUpdateFn);
   }
 }
 
