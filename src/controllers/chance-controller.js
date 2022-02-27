bf7e45625c95cbd7919e847503914a2d7384e09f
LB Johnston <lb.johnston@virginaustralia.com>
2022-02-27 19:58:30 +1000

    add a chance-controller (for fun)

---
+++ src/controllers/chance-controller.js
@@ -0,0 +1,28 @@
+import { Controller } from '@hotwired/stimulus';
+import Chance from 'chance';
+
+/**
+ * Just a fun controller to put random content into a controlled
+ * element.
+ */
+export default class extends Controller {
+  static values = {
+    seed: { default: `${Math.random() * 10000}`, type: String },
+    type: { default: 'paragraph', type: String },
+  };
+
+  static targets = ['element'];
+
+  connect() {
+    if (!this.seedValue) this.seedValue = new Date().toISOString();
+    const element = this.hasElementTarget ? this.elementTarget : this.element;
+    const seed = this.seedValue;
+    const type = this.typeValue;
+
+    const chance = new Chance(seed);
+
+    const textContent = chance[type]();
+
+    element.textContent = textContent;
+  }
+}
