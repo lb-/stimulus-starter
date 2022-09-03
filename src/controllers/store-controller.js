38017941e4b89fd25a612f304f6a00f238106a4f
LB Johnston <lb.johnston@virginaustralia.com>
2022-09-03 21:28:14 +1000

    move reducer into module script

--- src/controllers/store-controller.js
+++ src/controllers/store-controller.js
@@ -1,9 +1,17 @@
 import { Controller } from '@hotwired/stimulus';
+// import { reducer } from 'reducer';
 
 class StoreController extends Controller {
+  static values = { initial: { type: Object, default: {} } };
+
   initialize() {
     this.getState = this.getState.bind(this);
-    this._state = JSON.parse(this.element.textContent);
+    this._state = this.initialValue;
+    this._reducer = this.element.reducer;
+    setTimeout(() => {
+      // send update to subscribers after all elements load
+      this.updated();
+    });
     console.log(this.element, this._state);
   }
 
@@ -14,20 +22,15 @@ class StoreController extends Controller {
   receive(event) {
     console.log('receive', { event, state: this._state });
     const { currentTarget, detail = {} } = event;
-    if (this.reducer(detail)) {
+    const newState = this._reducer(this.getState(), detail);
+    if (newState) {
+      this._state = newState;
       this.updated(currentTarget);
     }
   }
 
-  reducer({ type }) {
-    if (type === 'counter/increment') {
-      this._state.counter += 1;
-      return true;
-    }
-  }
-
   /**
-   * sends an update event
+   * sends updates to subscribers
    */
   updated(target = document.body) {
     this.dispatch('subscribe', {
